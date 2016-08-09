057453cf317291abc1ac6f523073829d794dbfd1
Jeff Escalante <jescalan@users.noreply.github.com>
2016-08-09 18:44:40 -0400

    fresh start

---
+++ README.md
@@ -0,0 +1,66 @@
+# Reshape Include
+
+[![npm](https://img.shields.io/npm/v/reshape-include.svg?style=flat-square)](https://npmjs.com/package/reshape-include)
+[![tests](https://img.shields.io/travis/reshape/include.svg?style=flat-square)](https://travis-ci.org/reshape/include?branch=master)
+[![dependencies](https://img.shields.io/david/reshape/include.svg?style=flat-square)](https://david-dm.org/reshape/include)
+[![coverage](https://img.shields.io/coveralls/reshape/include.svg?style=flat-square)](https://coveralls.io/r/reshape/include?branch=master)
+
+Include other html files ("partials") into your html
+
+### Install
+
+```sh
+npm i reshape-include --save
+```
+
+### Usage
+
+Given the following input files:
+
+```html
+<!-- index.html -->
+<p>Here's my partial:</p>
+<include src='_partial.html'></include>
+<p>after the partial</p>
+```
+
+```html
+<!-- _partial.html -->
+<strong>hello from the partial!</strong>
+```
+
+Process them with reshape:
+
+```js
+const {readFileSync} = require('fs')
+const reshape = require('reshape')
+const include = require('reshape-include')
+
+const html = readFileSync('index.html')
+
+reshape({ plugins: include() })
+  .process(html)
+  .then((result) => console.log(result.output()))
+```
+
+Output:
+
+```html
+<p>Here's my partial:</p>
+<strong>hello from the partial!</strong>
+<p>after the partial</p>
+```
+
+### Options
+
+All options are optional, none are required.
+
+| Name | Description | Default |
+| ---- | ----------- | ------- |
+| **root** | Root path to resolve the include from | the file's path |
+| **addDependencyTo** | Object with addDependency() method, taking file path as an argument. Called whenever a file is included | |
+
+### License
+
+- Licensed under [MIT](LICENSE.md)
+- See our [contributing guide](contributing.md)
---
+++ test/test.js
@@ -0,0 +1,62 @@
+const include = require('..')
+const reshape = require('reshape')
+const test = require('ava')
+const path = require('path')
+const {readFileSync} = require('fs')
+const fixtures = path.join(__dirname, 'fixtures')
+
+test('default include html', (t) => {
+  return compare(t, 'basic')
+})
+
+test('root option', (t) => {
+  return compare(t, 'rootOption', { root: path.join(fixtures, 'partials') })
+})
+
+test('addDependencyTo option', (t) => {
+  const includePath = path.join(fixtures, 'partials/button.html')
+
+  return process('basic', {
+    addDependencyTo: { addDependency: (p) => t.truthy(p === includePath) }
+  })
+})
+
+test('include with no src errors', (t) => {
+  return process('no_src')
+    .catch((err) => {
+      t.truthy(err.toString().match(/include tag has no "src" attribute/))
+    })
+})
+
+test('invalid dependency add object errors', (t) => {
+  t.throws(() => include({ addDependencyTo: 1 }), '[reshape-include] "addDependencyTo" does not have an "addDependency" method')
+})
+
+test('correctly reports source filename', (t) => {
+  const inputFile = path.join(fixtures, 'basic.html')
+  const trackAst = (tree) => {
+    t.truthy(tree[0].content[1].location.filename.match(/partials\/button\.html/))
+    return tree
+  }
+
+  return reshape({
+    plugins: [include(), trackAst],
+    filename: inputFile
+  }).process(readFileSync(inputFile, 'utf8'))
+})
+
+function process (name, options = {}) {
+  const inputFile = path.join(fixtures, `${name}.html`)
+  return reshape({ plugins: include(options), filename: inputFile })
+    .process(readFileSync(inputFile, 'utf8'))
+}
+
+function compare (t, name, options = {}) {
+  const inputFile = path.join(fixtures, `${name}.html`)
+  const input = readFileSync(inputFile, 'utf8')
+  const expected = readFileSync(path.join(fixtures, `${name}.expected.html`), 'utf8')
+
+  return reshape({ plugins: include(options), filename: inputFile })
+    .process(input)
+    .then((res) => t.truthy(expected === res.output()))
+}
